2f50c809f120d8fae7b82345330970899747de45
Blaine McMahon <blainemcmahonuml@gmail.com>
2025-10-24 18:55:47 -0400

    Make rectangle move up,down,left,right

--- main.cpp
+++ main.cpp
@@ -14,18 +14,29 @@ int main() {
     const int xAxis {800};
     const int yAxis {600};
 
-    float xPos = random(800);
-    float yPos = random(600);
+    float xPos = random((xAxis/2));
+    float yPos = random((yAxis/2));
+
+    float velocity {0.1};
 
     sf::RenderWindow window(sf::VideoMode({xAxis,yAxis}), "Snake");
+
+    enum Direction {
+        UP,    // Default value is 0
+        DOWN,  // Default value is 1
+        LEFT,  // Default value is 2
+        RIGHT, // Default value is 3
+    };
+
+    Direction myDirection {LEFT};
 
     // Create rectangle and position it randomly within the grid
     sf::RectangleShape rectangle({10.0f, 10.0f});
     rectangle.setPosition({xPos, yPos});
-
     // Set the frame rate to 60 which is good enough for snake
     window.setFramerateLimit(60);
-    
+    sf::Vector2f movePosition {0.0, 0.0};
+
     // Run the window as long as the window is open
     while (window.isOpen())
     {
@@ -34,7 +45,60 @@ int main() {
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+        
+            if (event->is<sf::Event::KeyPressed>())
+            {
+                // Check for escape key
+                const auto keyEvent = event->getIf<sf::Event::KeyPressed>();
+                
+                if (keyEvent && keyEvent->code == sf::Keyboard::Key::Escape)
+                    window.close();
+
+                // Check for left key
+                if (keyEvent && keyEvent->code == sf::Keyboard::Key::Left)
+                {
+                    myDirection = Direction::LEFT;
+                }
+
+                // Check for right key
+                if (keyEvent && keyEvent->code == sf::Keyboard::Key::Right)
+                {
+                    myDirection = Direction::RIGHT;
+                }
+            
+                // Check for up key
+                if (keyEvent && keyEvent->code == sf::Keyboard::Key::Up)
+                {
+                    myDirection = Direction::UP;
+                }
+
+                if (keyEvent && keyEvent->code == sf::Keyboard::Key::Down)
+                {
+                    myDirection = Direction::DOWN;
+                }
+
+            }
+                
         }
+
+        switch (myDirection)
+        {
+            case UP:
+                movePosition = {0.0, -0.1};
+                break;
+            case DOWN:
+                movePosition = {0.0, 0.1};
+                break;
+            case LEFT:
+                movePosition = {-0.1, 0.0};
+                break;
+            case RIGHT:
+                movePosition = {0.1, 0.0};
+                break;
+        };
+
+        // Always move rectangle 
+        rectangle.move(movePosition);
 
         // Clear the window with black color
         window.clear(sf::Color::Black);
